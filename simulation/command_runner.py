b37ae0db7ba04e050f333d540dd7e2ae53e9c88f
harsh2370 <harshwarutkar6@gmail.com>
2026-06-18 16:07:25 +0530

    Improved natural command parsing for robot actions

--- simulation/command_runner.py
+++ simulation/command_runner.py
@@ -1,11 +1,60 @@
-def parse_command(command):
-    words = command.lower().split()
+from world import OBJECTS, LOCATIONS
 
-    if len(words) == 3 and words[0] == "move":
+
+def parse_command(command):
+    command = command.lower()
+
+    # Remove simple extra words
+    words = command.replace(".", "").replace(",", "").split()
+
+    available_objects = list(OBJECTS.keys())
+    available_locations = list(LOCATIONS.keys())
+
+    detected_object = None
+    detected_location = None
+
+    # Detect object name from command
+    for obj in available_objects:
+        if obj in words:
+            detected_object = obj
+            break
+
+    # Detect location name from command
+    for loc in available_locations:
+        if loc in words:
+            detected_location = loc
+            break
+
+    # Detect action words
+    move_words = ["move", "place", "put", "bring", "transfer"]
+
+    detected_action = None
+
+    for word in words:
+        if word in move_words:
+            detected_action = "move"
+            break
+
+    # Convert natural command into task plan
+    if detected_action == "move" and detected_object and detected_location:
         return [
-            {"action": "pick", "object": words[1]},
-            {"action": "place", "location": words[2]}
+            {
+                "action": "pick",
+                "object": detected_object
+            },
+            {
+                "action": "place",
+                "location": detected_location
+            }
         ]
 
-    print("Invalid command. Use format: move object location")
+    print("Could not understand command.")
+    print("Available objects:", available_objects)
+    print("Available locations:", available_locations)
+    print("Example commands:")
+    print("- move bottle tray")
+    print("- move the bottle to the tray")
+    print("- place cup left")
+    print("- put the box in the right")
+
     return []
